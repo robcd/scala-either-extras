4d3cf434c7b79268d0b05240c0d51f48ed45ffd0
Rob Dickens <rob.dickens@lafros.com>
2012-04-29 12:15:49 +0100

    now test 'check' with both bad and good values
    -  test("check (without mapping)") {
    +  test("check (without mapping) bad value") {
    +  test("check (without mapping) good value") {

--- src/test/scala/Tests.scala
+++ src/test/scala/Tests.scala
@@ -23,12 +23,17 @@ class Tests extends FunSuite with ShouldMatchers {
   def checkTrue(t: T): E = if (t._1 == true) Right(t) else Left("Boolean field was "+ t._1)
   def checkPositive(t: T): E = if (t._2 > 0) Right(t) else Left("Int field not > 0: "+ t._2)
 
-  test("check (without mapping)") {
+  test("check (without mapping) bad value") {
     val value: T = (false, -1)
     val expectedValue = {
       val msgs = List("Boolean field was "+ value._1, "Int field not > 0: "+ value._2)
       Left[List[String], T](msgs)
     }
+    value.check(checkTrue, checkPositive) should equal(expectedValue)
+  }
+  test("check (without mapping) good value") {
+    val value: T = (true, 1)
+    val expectedValue = Right[List[String], T](value)
     value.check(checkTrue, checkPositive) should equal(expectedValue)
   }
   ignore("checkAndMap") {
