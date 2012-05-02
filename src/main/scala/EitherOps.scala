ee72c53df2bb78a8207ea5e297326bef76115ca5
Rob Dickens <rob.dickens@lafros.com>
2012-05-02 14:18:50 +0100

    EitherOps: now contains EitherOps, and now compiles
    -trait ApplicativeFunctor[A, M[A]] extends Functor[A, M] {
    +trait EitherOps[L, R1] {
    
    -class EitherApplicativeFunctor[A](eitherA: Either[_, A])
    +object EitherOps {

--- src/main/scala/EitherOps.scala
+++ src/main/scala/EitherOps.scala
@@ -1,15 +1,17 @@
-/**
- * lets you apply a FunctionN to N values in N contexts */
-trait ApplicativeFunctor[A, M[A]] extends Functor[A, M] {
-  def <*>[B](mf: M[A => B]): M[B]
+trait EitherOps[L, R1] {
+  /**
+   * confers applicative-functor status: lets you apply a FunctionN to N values in N contexts. */
+  def <*>[R2](f: Either[L, R1 => R2]): Either[L, R2]
 }
 
-class EitherApplicativeFunctor[A](eitherA: Either[_, A])
-extends EitherFunctor(eitherA) with ApplicativeFunctor[A, ({type λ[A] = Either[_, A]})#λ] {
-  def <*>[B](eitherA2B: Either[Any, A => B]) = (eitherA, eitherA2B) match {
-    case (Left(x), _) => Left(x) // since, here, x is not of type A, and therefore may not be
-    // passed to f
-    case (Right(_), Left(x)) => Left(x)
-    case (Right(a), Right(f)) => Right(f(a))
+object EitherOps {
+  implicit def either2EitherOps[L, R1](either: Either[L, R1]): EitherOps[L, R1] =
+    new EitherOps[L, R1] {
+    def <*>[R2](f: Either[L, R1 => R2]) = (either, f) match {
+      case (Left(x), _) => Left(x) // since, here, x is not of type A, and therefore may not be
+      // passed to f
+      case (Right(_), Left(x)) => Left(x)
+      case (Right(a), Right(f)) => Right(f(a))
+    }
   }
 }
