db099ce1e2dcad1ab3c8520a23367b56ef1e8ef5
Rob Dickens <rob.dickens@lafros.com>
2012-05-02 20:30:00 +0100

    qed: !EitherOps: f and either swapped places !Tests: + test("putting the function first") {
    -trait EitherOps[L, R1] {
    +trait EitherOps[L, R1, R2] {
    
    -  def <*>[R2](f: Either[L, R1 => R2]): Either[L, R2]
    +  def <*>(either: Either[L, R1]): Either[L, R2]
    
     object EitherOps {
    -  implicit def either2EitherOps[L, R1](either: Either[L, R1]): EitherOps[L, R1] =
    -    new EitherOps[L, R1] {
    -      def <*>[R2](f: Either[L, R1 => R2]) = (either, f) match {
    +  implicit def eitherOfFunction2EitherOps[L, R1, R2](f: Either[L, R1 => R2]): EitherOps[L, R1, R2] =
    +    new EitherOps[L, R1, R2] {
    +      def <*>(either: Either[L, R1]) = (f, either) match {
    
    -        case (Right(r1), Right(f)) => Right(f(r1))
    +        case (Right(f), Right(r1)) => Right(f(r1))
    
    // test("that substituting Left results in a Left") {
    // test("combining checks on different types") {
    +  test("putting the function first") {

--- src/main/scala/EitherOps.scala
+++ src/main/scala/EitherOps.scala
@@ -1,16 +1,16 @@
-trait EitherOps[L, R1] {
+trait EitherOps[L, R1, R2] {
   /**
    * confers applicative-functor status: lets you apply a FunctionN to N values in N contexts. */
-  def <*>[R2](f: Either[L, R1 => R2]): Either[L, R2]
+  def <*>(either: Either[L, R1]): Either[L, R2]
 }
 
 object EitherOps {
-  implicit def either2EitherOps[L, R1](either: Either[L, R1]): EitherOps[L, R1] =
-    new EitherOps[L, R1] {
-      def <*>[R2](f: Either[L, R1 => R2]) = (either, f) match {
+  implicit def eitherOfFunction2EitherOps[L, R1, R2](f: Either[L, R1 => R2]): EitherOps[L, R1, R2] =
+    new EitherOps[L, R1, R2] {
+      def <*>(either: Either[L, R1]) = (f, either) match {
         case (Left(l), _) => Left(l)
         case (Right(_), Left(l)) => Left(l)
-        case (Right(r1), Right(f)) => Right(f(r1))
+        case (Right(f), Right(r1)) => Right(f(r1))
       }
     }
 }
