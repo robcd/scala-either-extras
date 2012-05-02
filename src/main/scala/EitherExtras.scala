66ee58efa6d8c423b8ac4ce76f2a99254317afe7
Rob Dickens <rob.dickens@lafros.com>
2012-05-02 11:37:50 +0100

    EitherExtras: checkAndMap no longer using var (as suggested by pr1001)
         def checkAndMap[L, R](checks: ((T) => Either[L, T])*)(f: (T) => R): Either[List[L], R] = {
    -      var msgs = List[L]()
    -      for {
    +      val msgs = for {
    -        check <- checks
    +        check <- checks.toList // from Seq (WrappedArray)
    -        msg <- check(any).left
    +        msg <- check(any).left.toSeq
    -      } msgs = msg::msgs
    +      } yield msg
    -      if (msgs.isEmpty) Right(f(any)) else Left(msgs.reverse)
    +      if (msgs.isEmpty) Right(f(any)) else Left(msgs)

--- src/main/scala/EitherExtras.scala
+++ src/main/scala/EitherExtras.scala
@@ -12,12 +12,11 @@ object EitherExtras {
     def check[L](checks: ((T) => Either[L, T])*): Either[List[L], T] =
       checkAndMap[L, T](checks: _*) { t => t }
     def checkAndMap[L, R](checks: ((T) => Either[L, T])*)(f: (T) => R): Either[List[L], R] = {
-      var msgs = List[L]()
-      for {
-        check <- checks
-        msg <- check(any).left
-      } msgs = msg::msgs
-      if (msgs.isEmpty) Right(f(any)) else Left(msgs.reverse)
+      val msgs = for {
+        check <- checks.toList // from Seq (WrappedArray)
+        msg <- check(any).left.toSeq
+      } yield msg
+      if (msgs.isEmpty) Right(f(any)) else Left(msgs)
     }
   }
 }
