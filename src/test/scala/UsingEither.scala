b2ec16b3ff4158b4503a14452f6f25073009260e
Rob Dickens <rob.dickens@lafros.com>
2012-05-05 11:31:19 +0100

    UsingEither: T was param, now member; f(a, b) now assigned to val res; test App added

--- src/test/scala/UsingEither.scala
+++ src/test/scala/UsingEither.scala
@@ -1,18 +1,30 @@
-trait UsingExceptions[T] {
+trait UsingExceptions {
+  type T
   def a: T
   def b: T
   def f(a: T, b: T): T
 
-  f(a, b)
+  val res = f(a, b)
 }
 
-trait UsingEither[L, T] {
+trait UsingEither[L] {
   this: EitherExtras[L] =>
+  type T
   type E[T] = Either[L, T]
   def a: E[T]
   def b: E[T]
   def f(a: T)(b: T): T
 
-  fast(f) <*> a <*> b
-  slow(f) <*> a <*> b
+  val res1 = fast(f) <*> a <*> b
+  val res2 = slow(f) <*> a <*> b
+}
+
+object test extends UsingEither[Exception] with EitherExtras[Exception] with App {
+  type T = Int
+  def a = Left(new Exception("couldn't obtain a"))
+  def b = Left(new Exception("couldn't obtain b"))
+  def f(a: T)(b: T) = a*b
+
+  println(res1)
+  println(res2)
 }
