4be8beb36837e6d6158d693c069b3d5d60f59409
Rob Dickens <rob.dickens@lafros.com>
2012-05-05 14:01:09 +0100

    UsingEither: def a, def b renamed def g, def h, to distinguish them from f arg names

--- src/test/scala/UsingEither.scala
+++ src/test/scala/UsingEither.scala
@@ -1,30 +1,31 @@
 trait UsingExceptions {
   type T
-  def a: T
-  def b: T
   def f(a: T, b: T): T
+  def g: T
+  def h: T
 
-  val res = f(a, b)
+  val res = f(g, h)
 }
 
 trait UsingEither {
   this: EitherExtras =>
   type T
   type E[T] = Either[L, T]
-  def a: E[T]
-  def b: E[T]
   def f(a: T)(b: T): T
+  def g: E[T]
+  def h: E[T]
 
-  val res1 = fast(f) <*> a <*> b
-  val res2 = slow(f) <*> a <*> b
+  val res1 = fast(f) <*> g <*> h
+  val res2 = slow(f) <*> g <*> h
 }
 
 object test extends UsingEither with EitherExtras with App {
   type L = Exception
   type T = Int
-  def a = Left(new Exception("couldn't obtain a"))
-  def b = Left(new Exception("couldn't obtain b"))
+
   def f(a: T)(b: T) = a*b
+  def g = Left(new Exception("couldn't obtain g"))
+  def h = Left(new Exception("couldn't obtain h"))
 
   println(res1)
   println(res2)
